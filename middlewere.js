6ab572772dbf3a5c3e7d6389485f89a267a6a9a4
Jestin Xavier <75082931+Jestinxavier@users.noreply.github.com>
2024-03-04 12:51:03 +0530

    new

---
+++ middlewere.js
@@ -0,0 +1,44 @@
+const KJUR = require('jsrsasign');
+require('dotenv').config();
+
+const generateSignat = async ()=>{
+console.log('====================================');
+console.log("ftyfffyttf");
+console.log('====================================');
+}
+const generateSignatureMiddleware = async(req, res, next) => {
+    console.log({body:req});
+    const { sessionName, role, sessionKey, userIdentity } = req.body;
+    const sdkKey = process.env.ZOOM_VIDEO_SDK_KEY;
+    const sdkSecret = process.env.ZOOM_VIDEO_SDK_SECRET;
+  
+    const iat = Math.round(new Date().getTime() / 1000) - 30;
+    const exp = iat + 60 * 60 * 2;
+    const oHeader = { alg: 'HS256', typ: 'JWT' };
+  
+    const oPayload = {
+      app_key: sdkKey,
+      tpc: sessionName,
+      role_type: role,
+      session_key: sessionKey,
+      user_identity: userIdentity,
+      version: 1,
+      iat: iat,
+      exp: exp
+    };
+  
+    const sHeader = JSON.stringify(oHeader);
+    const sPayload = JSON.stringify(oPayload);
+    
+    try {
+      const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, sdkSecret);
+      req.signature = signature; // Attach the signature to the request object
+      console.log(signature);
+      next(); // Pass control to the next middleware function
+    } catch (error) {
+      next(error); // Pass the error to the error-handling middleware
+    }
+  };
+
+
+module.exports = {generateSignat,generateSignatureMiddleware};
